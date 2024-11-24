28ca8e3e10a0d60c216c4cbaba806fc30e2642a5
Anhphap2004 <phapmeo2k4@gmail.com>
2024-11-25 01:07:00 +0700

    Update code, cập nhật quản lý tài khoản

--- Models/TbRole.cs
+++ Models/TbRole.cs
@@ -7,7 +7,7 @@ public partial class TbRole
 {
     public int RoleId { get; set; }
 
-    public string? RoleName { get; set; }
+    public required string RoleName { get; set; }
 
     public string? Description { get; set; }
 
